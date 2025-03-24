b302aa465d0d3df382af5fba3f4283f26a9fe446
Vladimir Ilic <vladimir.ilic101@gmail.com>
2025-03-24 10:41:28 +0100

    Sending data to Backend using HTTP PUT request

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core'
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core'
+import { map, catchError, throwError } from 'rxjs'
+import { HttpClient } from '@angular/common/http'
 
 import { PlacesContainerComponent } from '../places-container/places-container.component'
 import { PlacesComponent } from '../places.component'
+import { Place } from '../place.model'
 
 @Component({
   selector: 'app-user-places',
@@ -10,4 +13,39 @@ import { PlacesComponent } from '../places.component'
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {}
+export class UserPlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined)
+  isFetching = signal(false)
+  error = signal('')
+  private httpClient = inject(HttpClient)
+  private destroyRef = inject(DestroyRef)
+
+  ngOnInit(): void {
+    this.isFetching.set(true)
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/user-places')
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.error(error)
+          return throwError(() => new Error('Something went wrong! Please try again later...'))
+        }),
+      )
+      .subscribe({
+        next: (places) => {
+          this.places.set(places)
+        },
+        error: (error: Error) => {
+          this.error.set(error.message)
+        },
+        complete: () => {
+          // Complete function will run once this entire process is done
+          this.isFetching.set(false)
+        },
+      })
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe() // Not required for HttpClient
+    })
+  }
+}
